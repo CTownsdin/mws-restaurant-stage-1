e6912f78458d710863c0d44163b4326286e09116
Christian Townsdin <ctownsdin@expedia.org>
2018-07-24 20:26:08 -0700

    setup a service worker, and cache requests

--- js/main.js
+++ js/main.js
@@ -2,6 +2,25 @@ let restaurants, neighborhoods, cuisines
 var map
 var markers = []
 
+/* register service worker */
+registerServiceWorker = () => {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('../sw.js')
+        .then(
+          function(registration) {
+            console.log('ServiceWorker registration successful with scope: ', registration.scope)
+          },
+          function(err) {
+            console.log('ServiceWorker registration failed: ', err)
+          }
+        )
+    })
+  }
+}
+
+registerServiceWorker()
+
 /**
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
  */
